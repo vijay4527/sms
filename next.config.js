35eaa64433a4911e823fba7d799b595bab51cc22
vijay4527 <vijaysaroj4527@gmail.com>
2024-06-22 11:50:38 +0530

    image changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com","lamaro.like-themes.com"],
+    domains: ["images.pexels.com","lamaro.like-themes.com","fama.b-cdn.net"],
   },
 };
 
