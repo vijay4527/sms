42b6b5ae89b1a79df1eeffcfea0a01966b9f0c34
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-05 13:46:15 +0530

    script has been removed from the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,11 +47,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </AuthProvider>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossOrigin="anonymous"
-        ></script>
+        
       </body>
     </html>
   );
