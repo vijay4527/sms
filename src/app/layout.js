512539a7ea9d1d3c8ca2520deb94b85e9806423e
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-05 13:44:13 +0530

    script has been removed from head and added in the body

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,11 +39,7 @@ export default function RootLayout({ children }) {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossOrigin="anonymous"
         />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossOrigin="anonymous"
-        ></script>
+       
       </head>
       <body className={inter.className}>
         <AuthProvider>
@@ -51,6 +47,11 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </AuthProvider>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+          crossOrigin="anonymous"
+        ></script>
       </body>
     </html>
   );
