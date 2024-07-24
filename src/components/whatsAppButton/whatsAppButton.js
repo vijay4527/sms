2ebadb1f54eac6d352acdebab431001fa68f995b
vijay4527 <vijaysaroj4527@gmail.com>
2024-07-24 17:15:59 +0530

    button changes

--- src/components/whatsAppButton/whatsAppButton.js
+++ src/components/whatsAppButton/whatsAppButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./WhatsAppButton.module.css";
+import styles from "@/components/whatsAppButton/whatsAppButton.module.css";
 import Link from "next/link";
 const WhatsAppButton = () => {
   return (
